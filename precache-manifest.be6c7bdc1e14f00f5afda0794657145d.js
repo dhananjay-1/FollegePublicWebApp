4c450815d32b79d1ac92c9509ea10fc5b9eafab6
dhananjay <dhananjaydeswal11@gmail.com>
2021-01-06 10:58:40 +0530

    initial commit

---
+++ precache-manifest.be6c7bdc1e14f00f5afda0794657145d.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "50ec3c180ca1e7d58e46",
+    "url": "/FollegePublicWebApp/static/js/app.1c001bcd.chunk.js"
+  },
+  {
+    "revision": "7da9902bdc2a75bf1a26",
+    "url": "/FollegePublicWebApp/static/js/runtime~app.d7e28aef.js"
+  },
+  {
+    "revision": "780bdb90a23b35bddfda",
+    "url": "/FollegePublicWebApp/static/js/2.8fd0ac2b.chunk.js"
+  },
+  {
+    "revision": "b52357f89afdac4d6194ab45c23c968f",
+    "url": "/FollegePublicWebApp/static/media/Image.578c30e5.png"
+  },
+  {
+    "revision": "cd5d7b6e539262b9877872c609ab1301",
+    "url": "/FollegePublicWebApp/static/media/lgn.f00b8c0a.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/FollegePublicWebApp/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/FollegePublicWebApp/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/FollegePublicWebApp/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FollegePublicWebApp/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/FollegePublicWebApp/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/FollegePublicWebApp/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "e556623270777d5347fd4fb7239b74fd",
+    "url": "/FollegePublicWebApp/static/media/1.e23c4a74.png"
+  },
+  {
+    "revision": "a17f6db8288df0c8310ba71fc1ae0412",
+    "url": "/FollegePublicWebApp/static/media/2.ad0dfc1a.png"
+  },
+  {
+    "revision": "248bfbae3b5e0748e6de53a06e9aa7a9",
+    "url": "/FollegePublicWebApp/static/media/3.de030ff2.png"
+  },
+  {
+    "revision": "24735950d01cd2a8e477ac339a12b199",
+    "url": "/FollegePublicWebApp/static/media/phonefinal.00bb2738.png"
+  },
+  {
+    "revision": "ce82d8b384d8e2c59fac56e464a6d1e7",
+    "url": "/FollegePublicWebApp/static/media/collegeLife.b394defb.png"
+  },
+  {
+    "revision": "1e3960b45fa41b6383b8f34c181432d0",
+    "url": "/FollegePublicWebApp/./fonts/AlegreyaSans-Bold.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/FollegePublicWebApp/./fonts/Montserrat-SemiBold.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/FollegePublicWebApp/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/FollegePublicWebApp/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/FollegePublicWebApp/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/FollegePublicWebApp/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/FollegePublicWebApp/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/FollegePublicWebApp/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3b1b8ca3533c28658384613a35bdf9b3",
+    "url": "/FollegePublicWebApp/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/FollegePublicWebApp/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/FollegePublicWebApp/expo-service-worker.js"
+  },
+  {
+    "revision": "01a051a3b5212846431f00a1e0f64aa8",
+    "url": "/FollegePublicWebApp/register-service-worker.js"
+  },
+  {
+    "revision": "b7eb157b762779b30849c8fbf3d532e1",
+    "url": "/FollegePublicWebApp/static/js/2.8fd0ac2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/FollegePublicWebApp/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/FollegePublicWebApp/favicon-32.png"
+  },
+  {
+    "revision": "b27287954a8393ef9c70073b2fb0b988",
+    "url": "/FollegePublicWebApp/favicon.ico"
+  },
+  {
+    "revision": "05387ba88eb67ed3e2fc14f9e55804db",
+    "url": "/FollegePublicWebApp/manifest.json"
+  }
+];
